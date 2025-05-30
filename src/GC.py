850dc239a0d532c1f64a70a184f4942561958891
larsen-harvey <147673343+larsen-harvey@users.noreply.github.com>
2025-05-29 20:31:35 -0400

    up date gc

--- src/GC.py
+++ src/GC.py
@@ -1,7 +1,6 @@
 # Define the gravitational constant
 
-class G:
-    M = 1.989e30  # in kg
-    R = 6.9634e8  # in m
+class GravitationalConstants:
+    M = 1.989e30  # Mass of the Sun in kg
+    R = 6.9634e8  # Radius of the Sun in meters
     G = 6.67430e-11  # in m^3 kg^-1 s^-2    
-#G = 6.67430e-11  # in m^3 kg^-1 s^-2
